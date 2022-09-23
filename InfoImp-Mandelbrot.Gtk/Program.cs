8ddced91fe636791e986b602c425474bc94235b7
Tobias de Bruijn <t.debruijn@array21.dev>
2022-09-23 13:56:54 +0200

    Fixed windows

--- InfoImp-Mandelbrot.Gtk/Program.cs
+++ InfoImp-Mandelbrot.Gtk/Program.cs
@@ -2,6 +2,7 @@
 
 namespace InfoImp_Mandelbrot.Gtk {
     public class Program {
+        [STAThread]
         public static void Main(String[] args) {
             new Application().Run(new MainForm());
         }       
--- InfoImp-Mandelbrot.Wpf/Program.cs
+++ InfoImp-Mandelbrot.Wpf/Program.cs
@@ -2,6 +2,7 @@
 
 namespace InfoImp_Mandelbrot.Wpf {
     public class Program {
+        [STAThread]
         public static void Main(String[] args) {
             new Application().Run(new MainForm());
         }       
